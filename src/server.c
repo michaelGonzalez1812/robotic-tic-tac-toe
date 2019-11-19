8a1c301dddc502894b78f7b9476847def9f87739
Erick Cordero <eguicoro2@gmail.com>
2019-11-19 15:34:37 -0600

    ready

---
+++ src/server.c
@@ -0,0 +1,90 @@
+/*************************************************************
+ * Tecnológico de Costa Rica
+ * Conceptos básicos de Sistemas Operativos
+ * Athors:
+ *      Michael Gonzalez Rivera
+ *      Erick Cordero Rojas
+ *      Victor Montero
+ * 
+ * Description:
+ *      Server 
+ * ***********************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <limits.h>
+
+static void read_message (int fd, fd_set set) {
+  int bytes = 0;
+  char buf[PIPE_BUF] = { 0 };
+
+  FD_ZERO (&set);
+  FD_SET (fd, &set);
+
+  if ((select (fd + 1, &set, NULL, NULL, NULL)) < 1)
+    exit (1);
+  if (!FD_ISSET (fd, &set))
+    return;
+
+  if (read (fd, buf, PIPE_BUF) > 0)
+    //printf ("%s\n", buf);
+    command_identifier(buf);
+}
+
+//make the call to the library to move the robot
+void command_identifier(char commands[]){
+	int init_size = strlen(commands);
+  char delim1[] = ",";
+
+  //split to get the commands
+  char *ptr = strtok(commands, delim1);
+
+  char position = commands[7];
+  char logo = commands[18];
+
+	printf("%c\n", position); 
+	printf("%c\n", logo); 
+
+  printTicTacToe(position, logo);
+}
+
+void printTicTacToe(char position, char logo){
+  //port to connect
+  const char* serialport = "/dev/ttyACM0";
+  //baudio
+  int baud = 9600;
+  //file descriptor
+  int fd = -1;
+  //string to write
+  const char* str = "wwwwwwwwwwwwwwwwwwww\n\t";
+
+  //init conection
+  fd = serialport_init(serialport,baud);
+  //flush the wrong data
+  serialport_flush(fd);
+
+  if(strcmp(position, "0") == 0){
+    printf("sdfsdf");
+    //write to de fie
+    serialport_write(fd,str);
+  }
+  
+  //close the file
+  serialport_close(fd);
+}
+
+int main (void) {
+  fd_set set;
+  char *fifo = "/tmp/wspipeout.fifo";
+  int fd = 0;
+
+  if ((fd = open (fifo, O_RDWR | O_NONBLOCK)) < 0)
+    exit (1);
+  while (1){
+    read_message(fd, set);
+  }
+
+  return 0;
+}
